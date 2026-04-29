8e9b14d6054ef9cf9bef0a0cf898f759043c7efa
quothraven1122 <102286920+quothraven1122@users.noreply.github.com>
2026-04-29 16:42:16 +0900

    fix routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,15 @@ import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
 import Projects from "./pages/Projects";
+import ProjectDetail from "./pages/ProjectDetail";
 
 function App() {
   return (
     <Routes>
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/contact" element={<Contact />} />
-
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="projects" element={<Projects />} />
+        <Route path="projects/:id" element={<ProjectDetail />} />
         <Route path="contact" element={<Contact />} />
         <Route path="*" element={<NotFound />} />
       </Route>
